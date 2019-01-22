a06b056cfa04c13f2408a5529eca954910af17ca
bobby569 <bobby.yczh@gmail.com>
2019-01-22 09:31:41 -0500

    Simple done

--- SimpleApp/src/index.js
+++ SimpleApp/src/index.js
@@ -1,9 +1,5 @@
-const button = document.createElement('button');
-button.innerText = 'Click me';
-button.onclick = () => {
-	System.import('./image_view').then(module => {
-		module.default();
-	});
-};
+const button = document.createElement("button");
+button.innerText = "Click me";
+button.onclick = () => import("./image_view").then(module => module.default());
 
 document.body.appendChild(button);
--- SimpleApp/webpack.config.js
+++ SimpleApp/webpack.config.js
@@ -1,39 +1,44 @@
-const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-const config = {
-	entry: './src/index.js',
-	output: {
-		path: path.resolve(__dirname, 'build'),
-		filename: 'bundle.js',
-		publicPath: 'build/'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				use: 'babel-loader'
-			},
-			{
-				test: /\.css$/,
-				use: ExtractTextPlugin.extract({
-					fallback: 'style-loader',
-					use: 'css-loader'
-				})
-			},
-			{
-				test: /\.(jpe?g|png|gif|svg)$/,
-				use: [
-					{
-						loader: 'file-loader',
-						options: { limit: 40000 }
-					},
-					'image-webpack-loader'
-				]
-			}
-		]
-	},
-	plugins: [new ExtractTextPlugin('style.css')]
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js",
+    publicPath: "build/"
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader"
+        })
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: { limit: 40000 }
+          },
+          "image-webpack-loader"
+        ]
+      }
+    ]
+  },
+  plugins: [new ExtractTextPlugin("style.css")]
 };
-
-module.exports = config;
